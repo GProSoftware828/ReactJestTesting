0ef3e444322eaec8a4ee446e7e288efd35dada1c
GproSoftware828 <JetSendProductions@Gmail.com>
2019-10-03 18:55:59 +1000

    Initial commit

---
+++ button.js
@@ -0,0 +1,8 @@
+import { React, Component } from 'react';
+
+class Button extends Component {
+  render() {
+    return <p>Hi</p>;
+  }
+}
+export default Button;
---
+++ button.spec.js
@@ -0,0 +1,7 @@
+import { React } from 'react';
+import { TestUtils } from 'react-dom/test-utils';
+import { Button } from './button';
+
+TestUtils('works', () => {
+  expect(true).toBe(true);
+});
