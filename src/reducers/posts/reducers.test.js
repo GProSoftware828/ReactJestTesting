5ad8c4e267a81081a128057575747149472ae2ca
GproSoftware828 <JetSendProductions@Gmail.com>
2019-10-05 11:29:10 +1000

    Testing that reducer returns anything- FAIL

--- src/reducers/posts/reducers.test.js
+++ src/reducers/posts/reducers.test.js
@@ -1,1 +1,9 @@
 import { types } from '../../actions/types';
+import postsReducer from './reducer';
+
+describe('Posts Reducer', () => {
+  it('Should return default state', () => {
+    const newState = postsReducer(undefined, {});
+    expect(newState).toEqual([]);
+  });
+});
